87d7b78fa7e7a2e520348e634fdeca40a8300d8b
badcompwaifu <phasedkamo@gmail.com>
2022-02-15 20:32:24 -0300

    debug=true to see errors

--- wordle_clone/settings.py
+++ wordle_clone/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO-KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 ALLOWED_HOSTS = [
     '.herokuapp.com',
 ]
